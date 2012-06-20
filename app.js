2c5f16eab8f863930d6996daf4b53c3c38a762c5
Eric Hamilton <dilvie@dilvie.com>
2012-06-20 11:59:45 +0000

    Tweaked test deploy settings.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-    port = process.argv[2] || process.env.PORT || process.env.VCAP_APP_PORT || 8080;
+    port = process.env.PORT || process.env.VCAP_APP_PORT;
 
 http.createServer(function(request, response) {
 
