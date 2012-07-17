6928e7475060e85c4dc4c6ad681a9a534d3525a3
Eric Hamilton <dilvie@dilvie.com>
2012-07-17 07:23:29 -0600

    Added .forEach() shim.

--- o.js
+++ o.js
@@ -7,6 +7,16 @@
  */
 
 /*global exports */
+// Shim .forEach()
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function(fn, scope) {
+    for(var i = 0, len = this.length; i < len; ++i) {
+      fn.call(scope || this, this[i], i, this);
+    }
+  }
+}
+
+
 (function (exports) {
   'use strict';
   var namespace = 'odotjs',
