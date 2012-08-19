48f7e98b97558df317fbb7b82be7a537b5ab0304
Eric Hamilton <dilvie@dilvie.com>
2012-08-19 16:01:31 -0700

    Shim String.prototype.trim() for < IE8. Bump version.

--- o.js
+++ o.js
@@ -30,6 +30,12 @@ if (!Object.create) {
   };
 }
 
+// Shim String.prototype.trim()
+if(!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g,'');
+  };
+}
 
 (function (exports) {
   'use strict';
